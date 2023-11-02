ae1570fb0e0aee2c2f182a6801095b53b0234f74
felix <riehm@mpdl.mpg.de>
2023-11-02 16:42:28 +0100

    create base layout

---
+++ app/ui/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <nav className="h-[118px] bg-bloxberg-blue-950 px-4 py-6">
+            <div className="flex max-w-screen-xl">
+
+            </div>
+        </nav>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,27 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'bloxberg-black': '#000',
+        'bloxberg-white': '#fff',
+        'bloxberg-blue': {
+          100: '#BAD5D8',
+          200: '#9DC2CA',
+          300: '#7CA9AF',
+          400: '#6D9EA5',
+          500: '#5C8C98',
+          600: '#40727B',
+          700: '#2A5963',
+          800: '#29404f',
+          900: '#133944',
+          950: '#1c2b36',
+        },
+        'bloxberg-red': {
+          100: '#e01d6c',
+        },
+        'bloxberg-grey': {
+          100: '#EBE9E9',
+        },
       },
     },
   },
