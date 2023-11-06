9b5118ce2723fb017a30a8efba8de2f9149d2ef9
Felix Riehm <mail@felixriehm.de>
2023-11-06 19:47:38 +0100

    test commit

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -2,7 +2,6 @@ export default function Home() {
     return (
         <main className="flex flex-col items-center justify-between p-24 h-full bg-bloxberg-blue-800">
             <div className="bg-bloxberg-blue-800">
-
             </div>
         </main>
     )
