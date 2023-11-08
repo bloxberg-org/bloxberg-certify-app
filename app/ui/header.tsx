9dc7aee4167d9bfad377495b188b4e6cdf241596
Felix Riehm <mail@felixriehm.de>
2023-11-08 17:43:11 +0100

    change order of header items

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -7,25 +7,28 @@ export default function Header() {
         <nav className="h-[118px] bg-bloxberg-blue-950 px-4 py-6 flex justify-center items-center">
             <div className="flex max-w-screen-lg w-full justify-between items-center">
                 <Link href={`/`}>
-                    <Image
-                        src={bloxbergLogo}
-                        alt="bloxberg logo"
-                        className={"mt-5 mb-5 mr-5"}
-                        style={{
-                            height: '47px',
-                            width: 'fit-content',
-                        }}
-                    />
+                    <img src={bloxbergLogo.src} alt="bloxberg logo" className="mt-5 mb-5 mr-5 h-[47px] w-fit"></img>
+                    {/* requires nodejs server, for deploying without nodejs you would have to write a custom loader
+                     https://nextjs.org/docs/pages/building-your-application/deploying/static-exports#image-optimization*/}
+                    {/*<Image*/}
+                    {/*    src={bloxbergLogo}*/}
+                    {/*    alt="bloxberg logo"*/}
+                    {/*    className={"mt-5 mb-5 mr-5"}*/}
+                    {/*    style={{*/}
+                    {/*        height: '47px',*/}
+                    {/*        width: 'fit-content',*/}
+                    {/*    }}*/}
+                    {/*/>*/}
                 </Link>
                 <ul className="flex items-center">
-                    <Link href={`/verify`}>
-                        <li className="px-6 py-5 uppercase text-bloxberg-grey text-lg hover:text-bloxberg-red hover:cursor-pointer">
-                            Verify
-                        </li>
-                    </Link>
                     <Link href={`/certify`}>
                         <li className="px-6 py-5 uppercase text-bloxberg-grey text-lg hover:text-bloxberg-red hover:cursor-pointer">
                             Certify
+                        </li>
+                    </Link>
+                    <Link href={`/verify`}>
+                        <li className="px-6 py-5 uppercase text-bloxberg-grey text-lg hover:text-bloxberg-red hover:cursor-pointer">
+                            Verify
                         </li>
                     </Link>
                 </ul>
